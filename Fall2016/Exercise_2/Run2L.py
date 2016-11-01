e0eab6c1b3dad68e60707947d3dec7f26982c7fa
Jiahao Lin <jiahao.lin13@ic.ac.uk>
2016-11-01 17:59:28 +0000

    i

--- Fall2016/Exercise_2/Run2L.py
+++ Fall2016/Exercise_2/Run2L.py
@@ -24,7 +24,7 @@ def ConnectLayers(N1, N2):
 
   # Build network as a block matrix. Block [i,j] is the connection from
   # layer i to layer j
-  W = np.bmat([[np.zeros((N1,N1)), F*np.ones((N1,N2))], 
+  W = np.bmat([[np.zeros((N1,N1)), F*np.ones((N1,N2))],
                [np.zeros((N2,N1)),  np.zeros((N2,N2))]])
   D = Dmax*np.ones((N,N), dtype=int)
 
@@ -34,6 +34,18 @@ def ConnectLayers(N1, N2):
   b = 0.2*np.ones(N)
   c = -65 + 15*(r**2)
   d = 8 - 6*(r**2)
+
+  # Inhibitory
+  # a = 0.02*np.ones(N)
+  # b = 0.25*np.ones(N)
+  # c = -65 * r
+  # d = 2 * r
+
+  # Bursting
+  # a = 0.02 * np.ones(N)
+  # b = 0.2 * np.ones(N)
+  # c = -50 * r
+  # d = 2 * r
 
   net.setWeights(W)
   net.setDelays(D)
@@ -59,25 +71,32 @@ if __name__ == '__main__':
   I = np.hstack([5*np.ones(N1), np.zeros(N2)])
   T = np.arange(Tmin, Tmax + 1)
   V = np.zeros((len(T), N))
+  R = np.zeros(len(T))
 
   # Simulate
   for t in xrange(len(T)):
     net.setCurrent(I)
-    net.update()
+    fIdx = net.update()
+    R[t] = len(fIdx)
     V[t,:],_ = net.getState()
 
   # Plot results
   pl.figure()
-  ax1 = pl.subplot(211)
+
+  ax1 = pl.subplot(311)
+  ax1.plot(T, R)
+  ax1.set_ylabel('number of firing neurons')
+  ax1.set_title('raster')
+
+  ax1 = pl.subplot(312)
   ax1.plot(T, V[:, 0:N1])
   ax1.set_ylabel('Voltage (mV)')
   ax1.set_title('Layer 1')
 
-  ax2 = pl.subplot(212)
+  ax2 = pl.subplot(313)
   ax2.plot(T, V[:, (N1+1):N])
   ax2.set_xlabel('Time (ms)')
   ax2.set_ylabel('Voltage (mV)')
   ax2.set_title('Layer 2')
 
   pl.show()
-
