3d6f3ae8fefdc1f7310afdc4ab5ee081df2a88e9
Jiahao Lin <jiahao.lin13@ic.ac.uk>
2016-11-19 13:41:50 +0000

    refactor

--- Fall2016/Exercise_4/Answer.py
+++ Fall2016/Exercise_4/Answer.py
@@ -1,234 +1,220 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
 """
 Computational Neurodynamics
-Exercise 4
+Exercise 4 (Course Work)
 
 Jiahao Lin 2016
 """
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
 
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.random as rn
-# from IzNetwork import IzNetwork
-from NetworkWattsStrogatz import NetworkWattsStrogatz
-import IzNetwork as iz
+from IzNetwork import IzNetwork
 
-def BackgroundNoise():
-    lam = 0.01
-    samples = np.random.binomial(1, lam, 800)
+
+def BackgroundNoise(ExiNumPerMod, moduleNum):
+    lamda = 0.01
+    samples = rn.binomial(1, lamda, ExiNumPerMod*moduleNum)
     currents = np.zeros([1000, ])
-    currents[0:800] = map(lambda x: 15.0 if x > 0.0 else 0.0, samples)
+    currents[0:ExiNumPerMod*moduleNum] = map(lambda x: 15.0 if x > 0.0
+                                             else 0.0, samples)
     return currents
 
-def Network(ExiNumPerMod, ExiModConnNum, EtoIW):
-    AA = np.zeros([ExiNumPerMod, ExiNumPerMod])
-    connections = np.random.choice(10000, size=ExiModConnNum, replace=False)
-    for ii in range(ExiModConnNum):
-        vv = connections[ii]
-        xindex = vv/ExiNumPerMod
-        yindex = vv%ExiNumPerMod
-        # if xindex != yindex:
-        AA[xindex, yindex] = 17.0
-    return AA
 
-def ConnectLayers(p):
-  """
-  Create a network with 800 excitatory Izhikevich neurons with 200
-  Inhibitory neurons, respectively.
-  """
-  moduleNum = 8
-  ExiNumPerMod = 100
-  ExiModConnNum = 1000
-  InhiNum = 200
-  N = moduleNum * ExiNumPerMod + InhiNum
-  Dmax = 20  # Max synaptic delay, in ms
-  net = iz.IzNetwork(N, Dmax)
+def BuildExcitoryModule(ExiNumPerMod, ExiModConnNum, EtoEW):
+    Module = np.zeros([ExiNumPerMod, ExiNumPerMod])
+    connections = rn.choice(10000, size=ExiModConnNum, replace=False)
+    for i in range(ExiModConnNum):
+        connection = connections[i]
+        xindex = connection / ExiNumPerMod
+        yindex = connection % ExiNumPerMod
+        if xindex != yindex:
+            Module[xindex, yindex] = EtoEW()
+    return Module
 
-  EtoESF = 17.0
-  EtoISF = 50.0
-  ItoESF = 2.0
-  ItoISF = 1.0
 
-  EtoEW = lambda:1.0
-  EtoIW = lambda:50.0 * np.random.rand()
-  ItoEW = lambda:-2.0 * np.random.rand()
-  ItoIW = lambda:-np.random.rand()
+def BuildNetwork(rewiringProb):
+    """
+    Create a network with ExiNumrewiringProberMod*moduleNum excitatory
+    Izhikevich neurons with 200 Inhibitory neurons, respectively.
+    """
+    moduleNum = 8
+    ExiNumPerMod = 100
+    ExiModConnNum = 1000
+    InhiNum = 200
+    InhiToExciNum = 4
+    N = moduleNum * ExiNumPerMod + InhiNum
+    Dmax = 20  # Max synaptic delay, in ms
+    net = IzNetwork(N, Dmax)
 
-  EtoECD = lambda i, j:np.random.randint(1, 20, dtype="int")
-  EtoICD = lambda:1
-  ItoECD = lambda:1
-  ItoICD = lambda:1
+    EtoESF = 17.0
+    EtoISF = 50.0
+    ItoESF = 2.0
+    ItoISF = 1.0
 
-  Modules = np.zeros([8, 100, 100])
-  EtoEMatrix = np.zeros([ExiNumPerMod*moduleNum, ExiNumPerMod*moduleNum])
-  for i in range(moduleNum):
-      A = Network(ExiNumPerMod, ExiModConnNum, EtoIW)
-    #   plt.matshow(A)
-    #   plt.show()
-    #   print "A shape: ", A.shape
-      Modules[i, :, :] = A
-      startX = i*ExiNumPerMod
-      endX = startX + 100
-      EtoEMatrix[startX: endX, startX: endX] = A
+    EtoEW = lambda: 1.0
 
-  B = EtoEMatrix[0: 800, 0: 800]
-  # print "Modules shape: ", Modules.shape
-  for i in range(8):
-     m = Modules[i]
-     for xx in range(100):
-        for yy in range(100):
-            # print m
-            if m[xx, yy] > 0.0:
-                # print "pp: ", p
-                if np.random.rand() < p:
-                    # print "rewire"
-                    mNum = np.random.randint(0, 8, dtype="int")
-                    while mNum == i:
-                        mNum = np.random.randint(0, 8, dtype="int")
-                    WireNum = np.random.randint(0, 100, dtype="int")
-                    B[i*100+xx, i*100+yy] = 0.0
-                    # print ">> ",mNum, WireNum, mNum*800+WireNum
-                    B[i*100+xx, mNum*100+WireNum] = 17.0
+    EtoIW = lambda: rn.rand()
 
-  EtoEMatrix[0: 800, 0: 800] = B
+    ItoEW = lambda: -rn.rand()
 
-  EtoIMatrix = np.zeros([ExiNumPerMod*moduleNum, InhiNum])
-  EtoIs = np.zeros([800, 200])
-  cc = 0
-  ccs = np.random.choice(200, size=200, replace=False)
-  for i in range(200):
-      c = ccs[i]
-      for jj in range(4):
-          EtoIs[4*c+jj, i] = EtoIW()
-      cc += 1
-  # print("EtoIs shap: ", EtoIs.shape)
+    ItoIW = lambda: -rn.rand()
 
-  for o in range(800):
-      f = False
-      for ino in range(200):
-          if EtoIs[o, ino] != 0.0:
-              if not f:
-                  f = True
-              else:
-                  print "fff"
+    # Build Excitatory modules
+    Modules = np.zeros([moduleNum, ExiNumPerMod, ExiNumPerMod])
+    EtoEMatrix = np.zeros([ExiNumPerMod*moduleNum, ExiNumPerMod*moduleNum])
+    for i in range(moduleNum):
+        Module = BuildExcitoryModule(ExiNumPerMod, ExiModConnNum, EtoEW)
+        Modules[i, :, :] = Module
+        startX = i * ExiNumPerMod
+        endX = startX + ExiNumPerMod
+        EtoEMatrix[startX: endX, startX: endX] = Module
 
-  EtoIMatrix = EtoIs
+    # Rewiring Excitatory modules
+    EtoEMx = EtoEMatrix[0: ExiNumPerMod*moduleNum, 0: ExiNumPerMod*moduleNum]
+    for i in range(moduleNum):
+        originalModule = Modules[i]
+        for xCor in range(ExiNumPerMod):
+            for yCor in range(ExiNumPerMod):
+                if originalModule[xCor, yCor] > 0.0:
+                    if rn.rand() < rewiringProb:
+                        moduleIndex = rn.randint(0, 8, dtype="int")
+                        while moduleIndex == i:
+                            moduleIndex = rn.randint(0, 8, dtype="int")
+                        NeuronIndex = rn.randint(0, 100, dtype="int")
+                        xIndex = i*ExiNumPerMod + xCor
+                        yIndex = i*ExiNumPerMod + yCor
+                        # Delete original Wire
+                        EtoEMx[xIndex, yIndex] = 0.0
+                        # Rewire
+                        newYIndex = moduleIndex * ExiNumPerMod + NeuronIndex
+                        EtoEMx[xIndex, newYIndex] = EtoEW()
 
-  ItoEMatrix = np.zeros([InhiNum, ExiNumPerMod*moduleNum])
-  for i in range(InhiNum):
-     for j in range(ExiNumPerMod*moduleNum):
-         ItoEMatrix[i, j] = ItoEW()
+    EtoEMatrix[0: ExiNumPerMod*moduleNum, 0: ExiNumPerMod*moduleNum] = EtoEMx
 
-  ItoIMatrix = np.zeros([InhiNum, InhiNum])
-  for i222 in range(InhiNum):
-     for j222 in range(InhiNum):
-         ItoIMatrix[i222, j222] = ItoIW()
+    # Connect Excitatory to Inhibitory Neurons
+    EtoIMatrix = np.zeros([ExiNumPerMod*moduleNum, InhiNum])
+    randomInhiIndex = rn.choice(InhiNum, size=InhiNum, replace=False)
+    for inhibitory in range(InhiNum):
+        index = randomInhiIndex[inhibitory]
+        for j in range(InhiToExciNum):
+            EtoIMatrix[4 * index + j, inhibitory] = EtoIW()
 
-  # Build network as a block matrix. Block [i,j] is the connection from
-  # layer i to layer j
-  # print "EtoEMatrix shape: ", EtoEMatrix.shape
-  # print "EtoIMatrix shape: ", EtoIMatrix.shape
-  # print "ItoEMatrix shape: ", ItoEMatrix.shape
-  # print "ItoIMatrix shape: ", ItoIMatrix.shape
-  W = np.bmat([[EtoEMatrix, EtoIMatrix],
-               [ItoEMatrix, ItoIMatrix]])
+    ItoEMatrix = np.zeros([InhiNum, ExiNumPerMod*moduleNum])
+    for i in range(InhiNum):
+        for j in range(ExiNumPerMod*moduleNum):
+            ItoEMatrix[i, j] = ItoEW()
 
-  W = np.array(W)
-  plt.matshow(W, vmin=-2.0, vmax=50.0)
-  plt.show()
-  # return 0
-  # print "w shape: ", W.shape
-  EtoECDMatrix = np.ones([1000, 1000], dtype="int")
-  for q in range(1000):
-      for e in range(1000):
-        #   print W[q, w]
-          if W[q, e] > 0.0:
-              if q < 800 and e < 800:
-                  dd = np.random.randint(0, 20, dtype="int")
-                  EtoECDMatrix[q, e] = EtoECDMatrix[q, e] + dd
-  # EtoECDMatrix = np.fromfunction(EtoECD, (ExiNumPerMod*moduleNum, ExiNumPerMod*moduleNum), dtype="double")
-  # print EtoECDMatrix
-  D = EtoECDMatrix
-  plt.matshow(D, vmin=1.0, vmax=20.0)
-  plt.show()
+    ItoIMatrix = np.zeros([InhiNum, InhiNum])
+    for i in range(InhiNum):
+        for j in range(InhiNum):
+            ItoIMatrix[i, j] = ItoIW()
 
-  # All neurons are heterogeneous excitatory regular spiking
-  As = np.zeros([1000, ])
-  Bs = np.zeros([1000, ])
-  Cs = np.zeros([1000, ])
-  Ds = np.zeros([1000, ])
-  for i in range(1000):
-    #   print is
-    #   r = rn.rand()
-      if i < 800:
-        # Exi
-        Ea = 0.02
-        Eb = 0.2
-        Ec = -65 #+ 15*(r**2)
-        Ed = 8 #- 6*(r**2)
-        As[i] = Ea
-        Bs[i] = Eb
-        Cs[i] = Ec
-        Ds[i] = Ed
-      else:
-        # Inhibitory
-        Ia = 0.02
-        Ib = 0.25
-        Ic = -65 #* r
-        Id = 2 #* r
-        As[i] = Ia
-        Bs[i] = Ib
-        Cs[i] = Ic
-        Ds[i] = Id
+    # Build network as a block matrix. Block [i,j] is the connection from
+    # neuron i to neuron j
+    WeightMatrix = np.bmat([[EtoESF * EtoEMatrix, EtoISF * EtoIMatrix],
+                            [ItoESF * ItoEMatrix, ItoISF * ItoIMatrix]])
 
-  # print As
-  # print Bs
-  # print Cs
-  # print Ds
-  # Bursting
-  # a = 0.02 * np.ones(N)
-  # b = 0.2 * np.ones(N)
-  # c = -50 * r
-  # d = 2 * r
+    WeightMatrix = np.array(WeightMatrix)
+    # Plot connectivity matrix
+    plt.matshow(WeightMatrix, vmin=-2.0, vmax=50.0)
+    plt.show()
 
-  net.setWeights(W)
-  net.setDelays(D)
-  net.setParameters(np.array(As), np.array(Bs), np.array(Cs), np.array(Ds))
+    CDMatrix = np.ones([N, N], dtype="int")
+    for i in range(N):
+        for j in range(N):
+            if WeightMatrix[i, j] > 0.0:
+                if i < ExiNumPerMod*moduleNum and j < ExiNumPerMod*moduleNum:
+                    randomDelay = rn.randint(0, 20, dtype="int")
+                    CDMatrix[i, j] = CDMatrix[i, j] + randomDelay
 
-  return net
+    # Plot connection delay matrix
+    # plt.matshow(CDMatrix, vmin=1.0, vmax=20.0)
+    # plt.show()
 
-def runSimulation(p):
-      """
-      Create and run the network with constant input.
-      """
-    #   print BackgroundNoise()
-    #   return 0
-      print "p: ", p
+    # Ea = 0.02
+    # Eb = 0.2
+    # Ec = -65
+    # Ed = 8
+    # Ia = 0.02
+    # Ib = 0.25
+    # Ic = -65
+    # Id = 2
+    # # All neurons are heterogeneous excitatory or inhibitory regular spiking
+    # EAs = [Ea] * (ExiNumPerMod*moduleNum)
+    # IAs = [Ia] * InhiNum
+    # As = np.hstack((EAs, IAs))
+    # EBs = [Eb] * (ExiNumPerMod*moduleNum)
+    # IBs = [Ib] * InhiNum
+    # Bs = np.hstack((EBs, IBs))
+    # ECs = [Ec] * (ExiNumPerMod*moduleNum)
+    # ICs = [Ic] * InhiNum
+    # Cs = np.hstack((ECs, ICs))
+    # EDs = [Ed] * (ExiNumPerMod*moduleNum)
+    # IDs = [Id] * InhiNum
+    # Ds = np.hstack((EDs, IDs))
+    As = np.zeros([1000, ])
+    Bs = np.zeros([1000, ])
+    Cs = np.zeros([1000, ])
+    Ds = np.zeros([1000, ])
+    for i in range(1000):
+        #   print is
+        # r = rn.rand()
+        if i < 800:
+            # Exi
+            Ea = 0.02
+            Eb = 0.2
+            Ec = -65 #+ 15*(r**2)
+            Ed = 8 #- 6*(r**2)
+            As[i] = Ea
+            Bs[i] = Eb
+            Cs[i] = Ec
+            Ds[i] = Ed
+        else:
+            # Inhibitory
+            Ia = 0.02
+            Ib = 0.25
+            Ic = -65 #* r
+            Id = 2 #* r
+            As[i] = Ia
+            Bs[i] = Ib
+            Cs[i] = Ic
+            Ds[i] = Id
 
-      Tmin = 0
-      Tmax = 1000
+    net.setWeights(WeightMatrix)
+    net.setDelays(CDMatrix)
+    net.setParameters(np.array(As), np.array(Bs), np.array(Cs), np.array(Ds))
 
-      # Construct the network
-      net = ConnectLayers(p)
-    #   return 0
-      # Set current and initialise arrays
-      N = 1000
-      I = BackgroundNoise()
-      T = np.arange(Tmin, Tmax + 1)
-      V = np.zeros((len(T), N))
-      R = np.zeros(len(T))
-      Rx = np.array([])
-      Ry = np.array([])
-      MeanFireX = [[]] * 8
-      MeanFireY = [[]] * 8
+    return net
 
-      # Simulate
-      c1 = 0
-      c2 = 0
-      net.setCurrent([15.0] * 1000)
-      for t in xrange(len(T)):
-        I = BackgroundNoise()
+
+def runSimulation(rewiringProb):
+    """
+    Create and run the network with constant input.
+    """
+    moduleNum = 8
+    ExiNumPerMod = 100
+    print "rewiringProb: ", rewiringProb
+
+    Tmin = 0
+    Tmax = 1000
+
+    # Construct the network
+    net = BuildNetwork(rewiringProb)
+    # Set current and initialise arrays
+    I = BackgroundNoise(ExiNumPerMod, moduleNum)
+    T = np.arange(Tmin, Tmax + 1)
+    Rx = np.array([])
+    Ry = np.array([])
+    MeanFireX = [[]] * 8
+    MeanFireY = [[]] * 8
+
+    # Simulate
+    c1 = 0
+    c2 = 0
+    for t in xrange(len(T)):
+        I = BackgroundNoise(ExiNumPerMod, moduleNum)
         net.setCurrent(I)
         fIdx = net.update()
         # print "index count: ", len(fIdx)
@@ -237,59 +223,49 @@ def runSimulation(p):
             c1 += 1
             if idx < 800:
                 c2 += 1
-                # print "idx: ", idx
-                # print "idx/100: ", idx/100
-                # plt.plot([t], [idx])
                 Rx = np.append(Rx, t)
                 Ry = np.append(Ry, idx)
                 MeanFireX[idx/100] = np.append(MeanFireX[idx/100], t)
                 MeanFireY[idx/100] = np.append(MeanFireY[idx/100], idx)
-                # R[t] = len(fIdx)
-                # V[t,:],_ = net.getState()
-      print "len: ",  Rx.shape, Ry.shape, c1, c2
-    #   plt.plot(Rx, Ry, "o")
-    #   plt.show()
-    #   return 0
-      MeanFireXXs = [[]] * 8
-      MeanFireYYs = [[]] * 8
-      for mInx in range(8):
-          Xc = MeanFireX[mInx]
-        #   print "Xc len: ", Xc.shape
-          Yc = MeanFireY[mInx]
-        #   print "Yc len: ", Yc.shape
-          for windowIndex in range(50):
-              startT = 20 * windowIndex
-              endT = startT + 50
-              countPoint = 0
-              for v in range(len(Xc)):
-                  if Xc[v] >= startT and Xc[v] < endT:
-                      countPoint += 1
-              MeanFireXXs[mInx] = np.append(MeanFireXXs[mInx], startT + 25)
-              MeanFireYYs[mInx] = np.append(MeanFireYYs[mInx], countPoint)
 
-      # Plot results
-      plt.figure()
+    print "len: ",  Rx.shape, Ry.shape, c1, c2
 
-      ax1 = plt.subplot(211)
-      ax1.plot(Rx, Ry, "o")
-      ax1.set_ylabel('Neuron number')
-      ax1.set_xlabel('Time(ms) + 0s')
-      ax1.set_xlim(0, 1000)
-      ax1.set_ylim(0, 800)
-      ax1.set_title('Raster')
+    MeanFireXXs = [[]] * 8
+    MeanFireYYs = [[]] * 8
+    for mInx in range(8):
+        Xc = MeanFireX[mInx]
+        for windowIndex in range(50):
+            startT = 20 * windowIndex
+            endT = startT + 50
+            countPoint = 0
+            for v in range(len(Xc)):
+                if Xc[v] >= startT and Xc[v] < endT:
+                    countPoint += 1
+            MeanFireXXs[mInx] = np.append(MeanFireXXs[mInx], startT + 25)
+            MeanFireYYs[mInx] = np.append(MeanFireYYs[mInx], countPoint)
 
-      ax1 = plt.subplot(212)
-      for mIdnx in np.arange(8):
-          ax1.plot(MeanFireXXs[mIdnx], MeanFireYYs[mIdnx])
-      ax1.set_ylabel('Mean firing rate')
-      ax1.set_xlim(0, 1000)
-    #   ax1.set_ylim(0, 10)
-      ax1.set_title('Time(ms) + 0s')
+    # Plot results
+    plt.figure()
 
-      plt.show()
-      return 0
+    ax1 = plt.subplot(211)
+    ax1.plot(Rx, Ry, "o")
+    ax1.set_ylabel('Neuron number')
+    ax1.set_xlabel('Time(ms) + 0s')
+    ax1.set_xlim(0, 1000)
+    ax1.set_ylim(0, 800)
+    ax1.set_title('Raster')
+
+    ax1 = plt.subplot(212)
+    for mIdnx in np.arange(8):
+        ax1.plot(MeanFireXXs[mIdnx], MeanFireYYs[mIdnx])
+    ax1.set_ylabel('Mean firing rate')
+    ax1.set_xlim(0, 1000)
+    ax1.set_title('Time(ms) + 0s')
+
+    plt.show()
+    return 0
 
 if __name__ == "__main__":
-    ps = np.linspace(0, 0.5, 6)
-    for p in ps:
-    runSimulation(p)
+    rewiringProbabilities = np.linspace(0, 0.5, 6)
+    # for rewiringP in rewiringProbabilities:
+    runSimulation(0.0)
